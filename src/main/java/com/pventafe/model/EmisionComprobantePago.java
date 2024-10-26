284b4b684494b4b2371df410776d6a573884b5f6
RobinUnfv <2010001659@unfv.edu.pe>
2024-10-25 22:55:29 -0500

    Validando noCia, noCliente, total de valor venta y items de ventas

--- src/main/java/com/pventafe/model/EmisionComprobantePago.java
+++ src/main/java/com/pventafe/model/EmisionComprobantePago.java
@@ -1,10 +1,14 @@
 package com.pventafe.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmisionComprobantePago {
     private Arpfoe arpfoe;
     private Arfafe arfafe;
--- src/main/java/com/pventafe/presentation/controller/EmisionCompPagoContro.java
+++ src/main/java/com/pventafe/presentation/controller/EmisionCompPagoContro.java
@@ -2,6 +2,7 @@ package com.pventafe.presentation.controller;
 
 import com.pventafe.model.EmisionComprobantePago;
 import com.pventafe.model.EmisionComprobantePagoResponse;
+import com.pventafe.presentation.exception.BadRequestException;
 import com.pventafe.service.interfaces.IEmisionCompPagoServi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,19 @@ public class EmisionCompPagoContro {
     private IEmisionCompPagoServi iEmisionCpServi;
     @PostMapping
     public ResponseEntity<EmisionComprobantePagoResponse> emisionComprobantePago(@RequestBody EmisionComprobantePago emisionComprobantePago) {
-        System.out.println(emisionComprobantePago.getArpfoe().toString());
+        if (emisionComprobantePago.getArpfoe().getNoCia().length() != 2) {
+            throw new BadRequestException("El parámetro de compañia debe tener 2 caracteres");
+        }
+        if (emisionComprobantePago.getArpfoe().getNoCliente() == null ||
+                emisionComprobantePago.getArpfoe().getNoCliente().isEmpty() ) {
+            throw new BadRequestException("El parámetro de codigo de cliente no debe ser nulo o vacío");
+        }
+        if (emisionComprobantePago.getArpfoe().getTValorVenta() <= 0) {
+            throw new BadRequestException("El parametro de valor venta total debe de ser mayor a cero.");
+        }
+        if (emisionComprobantePago.getArpfoe().getArpfolList().isEmpty()) {
+            throw new BadRequestException("No tiene items de venta.");
+        }
         // Obtener URL de servicio
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{noCia}")
                 .buildAndExpand(emisionComprobantePago.getArpfoe().getNoCia()).toUri();
